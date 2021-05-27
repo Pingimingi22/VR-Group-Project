471e5d7f3566aa8dde43df74e5fafb564e5caa95
Dan <daniel@ac.com.au>
2021-05-27 22:44:25 +1000

    * Made some prototype folders, * Added super basic shooting/crosshair.

---
+++ Assets/Scripts/Managers/CombatManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// CombatManager maybe should be a singleton since we only want one. Probably implment this eventually.
+/// </summary>
+public class CombatManager : MonoBehaviour
+{
+
+    [Header("Cameras")]
+    public Camera m_mainCamera;
+    public Camera m_uiCamera;
+
+    [Header("Gun/Bullet Stats:")]
+    public float m_basicGunDamage = 10.0f;
+    public float m_bulletSpeed = 100.0f; // Maybe it will be hitscan?? So if it is hitscan I guess we wouldn't need a bullet speed. But for now I'll make it like this
+                                         // So we can see bullets moving across the screen.
+
+    [Header("Bullet Assets")]
+    public GameObject m_basicBullet;
+
+    [Header("Bullet Spawn Locations")]
+    public Transform m_spawnLeft;
+    public Transform m_spawnRight;
+
+
+    // test delete this.
+    public Vector3 testPos;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void Shoot(Vector3 crosshairPos)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(crosshairPos);
+
+        // Maybe we can make two bullets per shot for the pew pew effect?
+
+        GameObject newBullet1 = Instantiate(m_basicBullet);
+        newBullet1.transform.position = Vector3.zero;
+
+        Plane testPlane = new Plane(new Vector3(0, -1, 0), 10);
+        Ray testRay = new Ray(Vector3.zero, testPos - m_uiCamera.transform.position);
+
+
+
+        Rigidbody bullet1Rigidbody = newBullet1.GetComponent<Rigidbody>();
+        bullet1Rigidbody.AddForce(testRay.direction * m_bulletSpeed, ForceMode.Impulse);
+
+    }
+
+	private void OnDrawGizmos()
+	{
+        Ray testRay = new Ray(m_spawnLeft.position, testPos);
+        Gizmos.DrawRay(testRay);
+	}
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Player
+{ 
+    public class PlayerController : MonoBehaviour
+    {
+        // -------------------------- Public Inspector Variables -------------------------- //
+        [Header("Combat Manager")]
+        public CombatManager m_combatManager;
+
+        [Header("UI Camera")]
+        public Camera m_uiCamera;
+
+        [Header("Maybe useful stuff one day")]
+        public Transform m_rightControllerAnchor;
+        public Transform m_rightHandAnchor;
+
+        [Header("Crosshair things")]
+        public float m_crosshairSpeed = 100.0f; // idk sounds kinda normal?
+        
+        public Image m_crosshair;
+
+        // -------------------------------------------------------------------------------- //
+
+        Vector3 m_crosshairPos = Vector3.zero; // This is what moving the joysticks will be changing. We will then apply this to the image and other stuff.
+
+
+        float m_xDelta = 0;
+        float m_yDelta = 0;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            InitCrosshair();
+
+            
+        }
+    
+        // Update is called once per frame
+        void Update()
+        {
+            CrosshairInput();
+            UpdateCrosshairImage();
+
+            if (Input.GetAxis("Fire1") != 0)
+            {
+                // Shoot.
+                m_combatManager.Shoot(m_crosshairPos);
+            }
+
+
+
+            // test delete this
+            m_combatManager.testPos = m_crosshair.transform.position;
+        }
+
+        void CrosshairInput()
+        {
+            m_xDelta = Input.GetAxis("Horizontal");
+            m_yDelta = Input.GetAxis("Vertical");
+
+            if (m_xDelta != 0 || m_yDelta != 0)
+            {
+                // Player is moving joystick.
+                Vector3 direction = new Vector3(m_xDelta, m_yDelta, 0);
+                direction.Normalize();
+                m_crosshairPos += direction * m_crosshairSpeed * Time.deltaTime;
+            }
+        }
+
+        void UpdateCrosshairImage()
+        {
+            m_crosshair.transform.localPosition = m_crosshairPos;
+        }
+
+        /// <summary>
+        /// InitCrosshair() will just make sure that the crosshair starts in the centre of the screen.
+        /// </summary>
+        void InitCrosshair()
+        {
+            m_crosshair.transform.localPosition = Vector3.zero;
+        }
+    }
+}
