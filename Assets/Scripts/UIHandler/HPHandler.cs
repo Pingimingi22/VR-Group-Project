88ae1f820fb709b4ba90c5e6d6f233811c3c7d82
Dan <daniel@ac.com.au>
2021-05-30 10:59:54 +1000

    * Added simple health UI to help test VR headset things.

---
+++ Assets/Scripts/UIHandler/HPHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class HPHandler : MonoBehaviour
+{
+    public PlayerManager m_playerManager;
+    public TextMeshProUGUI m_text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_text.text = $"Health: {m_playerManager.m_playerHealth}";
+    }
+}
