5e916f262fd95ce303bb48b3e4e97878365ea5e5
Dan <daniel@ac.com.au>
2021-06-11 16:52:36 +1000

    * Added oxygen tank health bar.

--- Assets/Scripts/Input/InputHandler.cs
+++ Assets/Scripts/Input/InputHandler.cs
@@ -2,17 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class InputHandler : MonoBehaviour
+public class InputHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	//public static void Handle
 }
