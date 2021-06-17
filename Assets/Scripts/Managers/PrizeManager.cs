da3b8f3f4d8f4a8daa47a85c721dfde65b8c75d1
dan <daniel@ac.com.au>
2021-06-17 16:09:27 +1000

    Auto stash before merge of "main" and "origin/main"

--- Assets/Scripts/Managers/PrizeManager.cs
+++ Assets/Scripts/Managers/PrizeManager.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PrizeManager : MonoBehaviour
 {
+    public List<GameObject> prizes;
+    private int index = 0;
+    private int score = 0;
+    public int prizeIncrements = 600;
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (GameObject tropthy in prizes)
+        {
+
+            tropthy.active = false;
+        }
+
+        GameEvents.gameEvents.OnScoreEvent += AddPoints;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void AddPoints(int points)
     {
-        
+        score += points;
+        if (score >= prizeIncrements)
+        {
+            score -= prizeIncrements;
+
+            if (index < prizes.Count)
+            {
+                prizes[index].active = true;
+            }
+        }
     }
 }
